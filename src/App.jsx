9d8ff15287e2999219fbaa399a915102fa9dd503
mehediScriptDev <mehedi25122003@gmail.com>
2025-11-04 23:07:37 +0600

    chore: project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <button className='bg-white border border-gray-400 h-12 w-12 ml-1 leading-9 text-lg'>x</button>
+    </>
+  )
+}
+
+export default App
