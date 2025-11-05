e1771db0933fe9c4ffaf3b13f4080474f4fbca5a
mehediScriptDev <mehedi25122003@gmail.com>
2025-11-06 00:07:08 +0600

    feat: update game logic and add winner calculation for Tic-Tac-Toe

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,27 @@ function Squre({ value, onSquareClick }) {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (squares[i] || calculateWinner(squares)) return;
+    nextSquares[i] = xIsNext ? "X" : "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Squre value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Squre value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -41,3 +55,23 @@ function Board() {
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
