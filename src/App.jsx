d253c8f899195f73bc132e77e40c5a705560ef67
mehediScriptDev <mehedi25122003@gmail.com>
2025-11-05 23:30:50 +0600

    feat: implement Tic-Tac-Toe game structure and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
+import "./App.css";
 
-import './App.css'
-
-function App() {
-
+function Squre({ value, onSquareClick }) {
   return (
-    <>
-     <button className='bg-white border border-gray-400 h-12 w-12 ml-1 leading-9 text-lg'>x</button>
-    </>
-  )
+    <button
+      onClick={onSquareClick}
+      className="bg-white border border-gray-400 h-12 w-12 ml-1 leading-9 text-lg"
+    >
+      {value}
+    </button>
+  );
 }
 
-export default App
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+  return (
+    <>
+      <div className="flex">
+        <Squre value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Squre value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Squre value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="flex">
+        <Squre value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Squre value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Squre value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="flex">
+        <Squre value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Squre value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Squre value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default Board;
